2d9483c46f807c6b7d17facc988c223f3fa1930c
TamLe54 <tam.le@techsoft.asia>
2024-08-13 09:21:24 +0700

    chore: set up lint, prettier, tailwind, axios and react-query

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,66 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config({
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+    jest: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:tailwindcss/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
   files: ['**/*.{ts,tsx}'],
   ignores: ['dist'],
   languageOptions: {
     ecmaVersion: 2020,
     globals: globals.browser,
   },
-  plugins: {
-    'react-hooks': reactHooks,
-    'react-refresh': reactRefresh,
-  },
+  plugins: [
+    'react',
+    'react-hooks',
+    '@typescript-eslint',
+    'tailwindcss',
+    'simple-import-sort',
+  ],
   rules: {
-    ...reactHooks.configs.recommended.rules,
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    'no-console': 'warn',
+    'simple-import-sort/imports': 'warn',
+    'simple-import-sort/exports': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'off',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      1,
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
     ],
+    'tailwindcss/classnames-order': 'warn',
+    'tailwindcss/no-custom-classname': 'off',
+    'tailwindcss/no-contradicting-classname': 'error',
   },
 })
---
+++ src/contexts/tanstack-query/index.tsx
@@ -0,0 +1,41 @@
+import type { DefaultOptions } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactNode } from 'react';
+
+const queryConfig: DefaultOptions = {
+  queries: {
+    refetchOnWindowFocus: false,
+  },
+};
+
+export const appQueryClient = new QueryClient({ defaultOptions: queryConfig });
+
+export const TanstackQueryWrapper = ({ children }: { children: ReactNode }) => {
+  return (
+    <QueryClientProvider client={appQueryClient}>
+      {children}
+      <ReactQueryDevtools buttonPosition="bottom-right" />
+    </QueryClientProvider>
+  );
+};
+
+const mockQueryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // ✅ turns retries off
+      retry: false,
+    },
+  },
+});
+
+/**
+ * QueryWrapper support for testing tanstack-query hooks
+ * @returns
+ */
+export const mockQueryWrapper = () => {
+  // eslint-disable-next-line react/display-name
+  return ({ children }: { children: ReactNode }) => (
+    <QueryClientProvider client={mockQueryClient}>{children}</QueryClientProvider>
+  );
+};
