7e383cf12709f41eacc5a78bf2ecb24565b625ef
TamLe54 <tam.le@techsoft.asia>
2024-08-18 22:30:20 +0700

    fix: eslint and package

--- src/index.tsx
+++ src/index.tsx
@@ -19,6 +19,7 @@ declare module '@tanstack/react-router' {
   }
 }
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppInit>
